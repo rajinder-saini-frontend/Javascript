e7cc7902a50e0daec320ef1c3b88fa82a901a6ec
Rajinder Tpss <rajindersainitpss@gmail.com>
2022-03-09 09:30:10 +0530

    js array destructuring  and Values or variables

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// Array Destructuring
+document.querySelector('Array Destructuring')
+const arry=['one','two','three','four'];
+const [x,...rest] = arry;
+
+// Data Types
+
+num = 212;
+string = "Hello world";
+bool = true;
+boolF = false;
+// unD;
+nu = null;
+big = BigInt;
+
+console.log(num);
+console.log(string);
+console.log(bool);
+console.log(boolF);
+// console.log(unD);
+console.log(nu);
+console.log(big);
