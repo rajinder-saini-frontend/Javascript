39160ef99096a3fb70e0ef8bf81398eb93a1edbb
Rajinder Tpss <rajindersainitpss@gmail.com>
2022-03-09 11:17:19 +0530

    this is new code of javascript learning

--- js/main.js
+++ js/main.js
@@ -1,22 +1,89 @@
-// Array Destructuring
+/*// Array Destructuring
 document.querySelector('Array Destructuring')
 const arry=['one','two','three','four'];
 const [x,...rest] = arry;
 
 // Data Types
 
-num = 212;
-string = "Hello world";
-bool = true;
-boolF = false;
-// unD;
-nu = null;
+let num = 212,
+string = "Hello world",
+bool = true,
+boolF = false,
+unD,
+nu = null,
 big = BigInt;
 
-console.log(num);
-console.log(string);
-console.log(bool);
-console.log(boolF);
-// console.log(unD);
-console.log(nu);
-console.log(big);
+console.log(num, typeof num);
+console.log(string, typeof string);
+console.log(bool, typeof bool);
+console.log(boolF, typeof boolF);
+console.log(unD, typeof unD);
+console.log(nu, typeof nu);
+console.log(big, typeof big);
+
+
+const age = 2022 - 1993;
+console.log(age);
+*/
+
+// const, let, and var
+
+/*
+const marksHeight = 1.69;
+const marksWeight = 78;
+
+const johnHeight = 1.95;
+const johnWeight = 92
+
+const BMIf = marksWeight / marksHeight ** 2;
+const BMI = johnWeight / (johnHeight * johnHeight);
+
+console.log(BMIf);
+console.log(BMI);
+
+if(BMIf > BMI){
+    console.log(true);
+}
+*/
+
+
+// String
+/*
+const firstName = "Rajinder";
+const lastName = "Saini";
+
+const stringOut = "I'm " + firstName + " " + lastName ;
+
+console.log(stringOut);
+
+const newOut = `I'm ${firstName} ${lastName}`;
+
+console.log(newOut);
+
+console.log('new data type \n\
+this is new data \n\
+new data')
+
+console.log(
+`new data first line
+second line data
+last data line`
+)*/
+
+
+// if else Statement
+/*
+const age = 24;
+
+// const outAge = age >= 23;
+
+// if(outAge){
+//     console.log("Old enough to drike");
+// }
+
+if(age >= 25){
+    console.log("Old enough to drive a car");
+}
+else{
+    console.log("Grow Up first then drive a car");
+}*/
